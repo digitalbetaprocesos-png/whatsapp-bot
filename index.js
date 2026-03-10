76fc03213a3839ab68f27dd2755d1db858235a40
Beta Procesos <digitalbetaprocesos@gmail.com>
2026-03-10 09:29:30 -0600

    se cambio mensaje de protocolos

--- index.js
+++ index.js
@@ -634,7 +634,7 @@ await sendMessage(from,"¿Qué producto o servicio te interesa?");
 else if (userStates[from].step ==="cot_producto"){
   userStates[from].producto=msg;
   userStates[from].step="cot_comentarios"
-await sendMessage(from,"Escribe un comentario extra para tu cotizacion por ejemplo:\n ¿que tipo de superfice desea aplicar el servicio?\n¿Que tipo desea eliminar?")
+await sendMessage(from,"Escribe un comentario extra para tu cotizacion por ejemplo:\n¿Qué tipo de superfice desea aplicar el servicio?\n¿Qué tipo desea eliminar?")
 
 const nuevaCotizacion = new Cotizacion({
 
